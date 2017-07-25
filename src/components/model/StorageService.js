e13e9ba0663ba5c0d7cec868735cd668eb9bdf5d
Michael Dean <madean@directv.com>
2017-07-25 15:38:48 -0400

    Added title and a few movies.

--- src/components/model/StorageService.js
+++ src/components/model/StorageService.js
@@ -7,42 +7,8 @@ angular.module('md.app.model')
     .service('StorageService', function (
         pouchDB
     ) {
-
-        var db = pouchDB('movies');
-
         //private members
-        // var movies = [
-        //     {
-        //         title: 'Finding Nemo',
-        //         genre: 'Animation',
-        //         actors: 'Ellen DeGeneres',
-        //         rating: 5
-        //     },
-        //     {
-        //         title: 'Cars',
-        //         genre: 'Animation',
-        //         actors: 'Owen Wilson',
-        //         rating: 4
-        //     },
-        //     {
-        //         title: 'Toy Story',
-        //         genre: 'Animation',
-        //         actors: 'Tom Hanks',
-        //         rating: 4.5
-        //     },
-        //     {
-        //         title: 'Night at the Museum',
-        //         genre: 'Action & Adventure',
-        //         actors: 'Ben Stiller',
-        //         rating: 3.5
-        //     },
-        //     {
-        //         title: 'Step Brothers',
-        //         genre: 'Comedy',
-        //         actors: 'Will Ferrell',
-        //         rating: 4.5
-        //     }
-        // ];
+        var db = pouchDB('movies');
 
         var options = {
             /*eslint-disable camelcase */
